6b14b7830df299cf76e130bdcc07b5df2b85a786
Saaim Japanwala <92124191+sjapanwala@users.noreply.github.com>
2024-05-28 23:40:41 -0600

    Update builder.cmd

--- .packages/builder.cmd
+++ .packages/builder.cmd
@@ -3,4 +3,8 @@ chcp 65001>nul
 curl https://raw.githubusercontent.com/sjapanwala/unwrap-package-manager/main/.packages/unwrap.cmd -o temp_unwrap.cmd
 set file=unwrap.cmd
 powershell -command "(Get-Content "temp_unwrap.cmd") | Set-Content "unwrap.cmd"
-echo done
+del temp_unwrap.cmd
+echo unwrap package manager has finished installing!
+echo.
+echo use unwrap -? for help commands or visit the github!
+echo https://github.com/sjapanwala/unwrap-package-manager
