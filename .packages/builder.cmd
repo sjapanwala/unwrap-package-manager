bad2722414f81c78165c7f87880c492d87c37bb8
Saaim Japanwala <92124191+sjapanwala@users.noreply.github.com>
2024-05-28 23:39:16 -0600

    Update builder.cmd

--- .packages/builder.cmd
+++ .packages/builder.cmd
@@ -2,5 +2,5 @@
 chcp 65001>nul
 curl https://raw.githubusercontent.com/sjapanwala/unwrap-package-manager/main/.packages/unwrap.cmd -o temp_unwrap.cmd
 set file=unwrap.cmd
-powershell -command "(Get-Content "temp_unwrap") | Set-Content "unwrap.cmd"
+powershell -command "(Get-Content "temp_unwrap.cmd") | Set-Content "unwrap.cmd"
 echo done
