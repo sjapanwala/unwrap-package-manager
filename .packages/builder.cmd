c66c9231f239953ac71c091bf14bacedab850589
Saaim Japanwala <92124191+sjapanwala@users.noreply.github.com>
2024-05-28 23:35:19 -0600

    Create builder.cmd

---
+++ .packages/builder.cmd
@@ -0,0 +1,6 @@
+@echo off
+chcp 65001>nul
+curl https://raw.githubusercontent.com/sjapanwala/unwrap-package-manager/main/.packages/unwrap.cmd -o temp_unwrap.cmd
+set file=unwrap.cmd
+powershell -command "(Get-Content "temp_unwrap") | Set-Content "unwrap.cmd"
+echo done
