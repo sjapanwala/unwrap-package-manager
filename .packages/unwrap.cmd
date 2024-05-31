39b865ea68aa4926a3c4487461d6039cf478783b
Saaim Japanwala <92124191+sjapanwala@users.noreply.github.com>
2024-05-31 04:16:01 -0600

    update version 0.4.1 - check update logs 👍

--- .packages/unwrap.cmd
+++ .packages/unwrap.cmd
@@ -1,6 +1,7 @@
 @echo off
 chcp 65001>nul
 setlocal enabledelayedexpansion
+title Currently Unwrapping
 
 set cmdres=[0m[[91munwrap[0m]:
 :: get path
@@ -10,7 +11,6 @@ for /f "tokens=*" %%a in ('powershell -Command "(Get-Content 'C:\users\%username
 REM Sample string containing an object
 set "convert=%download_location%"
 
-REM Replace "fox" with "cat"
 set "converted_location=%convert:auser=!username!%"
 :: init commands
 :main
@@ -18,17 +18,15 @@ if "%1" == "-search" (
     if not "%2" == "" (
         set package_name=%2
         goto def_search_packages
-        echo %cmdres% Arguement Error: Package Name Not Defined
-        goto eof
     ) else (
-        echo %cmdres% Arguement Error: Package Name Not Defined
+        echo %cmdres% Arguement Error; Package Name Not Defined
         goto eof
     )
 )
 if "%1" == "-install" (
     set show_raw=False
     if "%2" == "" (
-        echo %cmdres% Arguement Error: Package Name Not Defined
+        echo %cmdres% Arguement Error; Package Name Not Defined
         goto eof
     ) else (
         if "%3" == "-raw" (
@@ -45,11 +43,11 @@ if "%1" == "-?" (
 
 if "%1" == "-repair" (
     if "%2" == "" (
-        echo %cmdres% Path Error: Please Provide A File inside "%converted_location%".
+        echo %cmdres% Path Error; Please Provide A File inside "%converted_location%".
         goto eof
     ) else (
         if not exist "%converted_location%\%2" (
-            echo %cmdres% Path Error: Please Prove A Path That Exists Inside "%converted_location%"
+            echo %cmdres% Path Error; Please Prove A Path That Exists Inside "%converted_location%"
             goto eof
         ) else (
             set repair_file_path=%converted_location%\%2
@@ -79,16 +77,42 @@ if "%1" == "-more" (
     goto def_show_more
 )
 
+if "%1" == "-remove" (
+    if not "%2" == "" (
+        set package_name=%2
+        goto def_remove_packages
+    ) else (
+        echo %cmdres% Arguement Error; No Package Name Defined
+        goto eof
+    )
+)
+
+if "%1" == "-updatelogs" (
+    echo.
+    curl -s https://raw.githubusercontent.com/sjapanwala/unwrap-package-manager/main/.visible/updates
+    goto eof
+)
+
+if "%1" == "-mypkg" (
+    if not "%2" == "" (
+        set package_name=%2
+        goto def_pkg_bool
+    ) else (
+        goto def_installed_packages
+    )
+)
+
+:: add arg headers before this
 if not "%1" == "" (
     goto defined_error
 ) else (
-    echo %cmdres% Arguement Error: No Command Provided
+    echo %cmdres% Arguement Error; No Command Provided
     echo Try "-?" for help and commands.
     goto eof
 )
 
 :defined_error
-echo %cmdres% Arguement Error: "%1" is Too Ambiguous
+echo %cmdres% Arguement Error; "%1" is Too Ambiguous
 echo Try "-?" for help and commands.
 goto eof
 
@@ -190,8 +214,43 @@ echo.
 curl -s %package_link%>"C:\users\%username%\.unwrap\.temp\%package_name%.unw"
 set FILE_PATH="C:\users\%username%\.unwrap\.temp\%package_name%.unw"
 for %%F in (%FILE_PATH%) do set FILE_SIZE=%%~zF
-echo The Following Package Will Be Installed,
-echo    %package_name%:    !FILE_SIZE![32mKB[0m
+
+
+:: read and convert sizes,
+set size=!FILE_SIZE!
+set trueSize =!FILE_SIZE!
+set finalSize=0
+set kb=1000
+set mb=1000000
+set gb=1000000000
+set unit=B
+if !size! gtr !gb! (
+    set /a finalSize=!size! / !gb!
+    set unit=GB
+    goto final_convert
+)
+if !size! gtr !mb! (
+    set /a finalSize=!size! / !mb!
+    set unit=MB
+    goto final_convert
+)
+if !size! gtr !kb! (
+    set /a finalSize=!size! / !kb!
+    set unit=KB
+    goto final_convert
+)
+set finalSize=!size!
+:final_convert
+set FILE_SIZE=!finalSize!
+:: get data for other file info
+for /f "usebackq tokens=*" %%a in (`powershell -command ^
+    "Get-ChildItem -Path '%converted_location%' -Recurse -File | Measure-Object -Property Length -Sum | Select-Object -ExpandProperty Sum"`) do (
+    set "targetSizeBefore=%%a"
+)
+
+echo The Following Package(s) Will Be Installed,
+echo    %package_name%:       !FILE_SIZE![32m!unit![0m
+echo    Destination:   !converted_location!
 :choose_cont
 set choice=""
 set /p choice=Do You Want To Continue? [y/n]: 
@@ -214,16 +273,36 @@ if errorlevel == 0 (
 goto eof
 
 :installfile
+if exist %converted_location%\%package_name%.cmd (
+    echo [31mScript Already Exists in [92m"%converted_location%"[0m
+    :choice
+    set choice=l
+    set /p choice=Overwrite? [y/n]: 
+    if %choice%==y goto Overwrite
+    if %choice%==Y goto Overwrite
+    if %choice%==n goto eof
+    if %choice%==N goto eof
+    goto choice
+)
+:Overwrite
 echo Installing to Path "%converted_location%"
 echo.
-PING -n 1 8.8.8.8 | FIND "TTL=">nul && echo [31m━━━━━━━━━━[0m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+title ━━━━━━━━━━
+PING -n 1 8.8.8.8 | FIND "TTL=">nul
 powershell -command "(Get-Content "C:\users\%username%\.unwrap\.temp\%package_name%.unw") | Set-Content "C:\users\%username%\.unwrap\.temp\%package_name%.cmd"
-PING -n 1 8.8.8.8 | FIND "TTL=">nul && echo [31m━━━━━━━━━━━━━━━━━━━[0m━━━━━━━━━━━━━━━━━━━━━
+PING -n 1 8.8.8.8 | FIND "TTL=">nul
+del "C:\users\%username%\.unwrap\.temp\%package_name%.unw"
+title ━━━━━━━━━━━━━━━━━━━
 move "C:\users\%username%\.unwrap\.temp\%package_name%.cmd" "%converted_location%">nul
-PING -n 1 8.8.8.8 | FIND "TTL=">nul && echo [31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━[0m━━━━━━
+PING -n 1 8.8.8.8 | FIND "TTL=">nul
+title ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+PING -n 1 8.8.8.8 | FIND "TTL=">nul
 echo memory>"C:\users\%username%\.unwrap\.temp\memory\%package_name%.mem"
 timeout 3 >nul 
 PING -n 1 8.8.8.8 | FIND "TTL=">nul && echo [31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━[0m
+title ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+PING -n 1 8.8.8.8 | FIND "TTL=">nul
+title Finished Installing
 echo.
 echo Finalizing Processes...
 PING -n 1 8.8.8.8 | FIND "TTL=">nul
@@ -245,17 +324,22 @@ goto eof
 ::curl -s https://raw.githubusercontent.com/sjapanwala/unwrap-package-manager/main/.visible/help
 echo Usage: unwrap ^<COMMAND^> [^<ARGS...^>]
 echo.
-echo COMMAND,  ARGS,
+echo COMMAND,   ARGS,
+echo.
 echo -?/-help                 shows the help menu, (this menu)
 echo -install [PKGNAME]       install packages from the unwrap script database
 echo -init                    initialize the package manager, this is required on first boot
-echo -upt                     update the packages to install
-echo -repair  [FILENAME]      use this command to repair the encoding (if a file doesnt work)
-echo -search  [PKGNAME]       search for packages, if none found, try "unwrap -upt"
-echo -pkgpush [FILENAME]      upload a package to a custom database, which can be sent to main packages
-echo                            (requires unwrapy.py package to be installed)
+echo -mypkg                   lists all packages you have installed and thier install Date
+echo -mypkg   [PKGNAME]       checks actuality of a specific package, returns a bool
+echo -pkgpush [FILENAME]      upload a package to a custom database, which can be sent to main package
 echo -pkglist                 lists all available packages, ~5mins from updates
-echo -more                    shows more information
+echo -remove  [PKGNAME]       removes a package thats installed, needs to be in "download_location"
+echo -repair  [FILENAME]      use this command to repair the encoding (if a file doesnt work)
+
+echo -search  [PKGNAME]       search for packages, if none found, try "unwrap -upt"
+
+echo -upt                     update the packages to install
+::echo -more                    shows more information
 goto eof
 
 :def_update
@@ -349,6 +433,62 @@ echo.
 curl -s https://raw.githubusercontent.com/sjapanwala/unwrap-package-manager/main/.visible/list-packages
 goto eof
 
+:def_remove_packages
+if not exist %converted_location%\%package_name%.cmd (
+    echo %cmdres% Locate Error; "%package_name%.cmd" Was Not Found
+) else (
+    ::set choice=
+    ::set /a choice=Remove %package_name%? [y/n] 
+    ::if %choice% == y goto cont_remove
+    ::if %choice% == Y goto cont_remove
+    ::if %choice% == N goto eof
+    ::if %choice% == n goto eof
+    :cont_remove
+    del %converted_location%\%package_name%.cmd>nul
+    PING -n 1 8.8.8.8 | FIND "TTL=">nul
+    echo Removing Main Program
+    PING -n 1 8.8.8.8 | FIND "TTL=">nul
+    del C:\users\%username%\.unwrap\.temp\memory\%package_name%.mem>nul
+    PING -n 1 8.8.8.8 | FIND "TTL=">nul
+    echo Removing Memory File
+    PING -n 1 8.8.8.8 | FIND "TTL=">nul
+    echo Removing Traces
+    PING -n 1 8.8.8.8 | FIND "TTL=">nul
+    echo Removing Paths
+    PING -n 1 8.8.8.8 | FIND "TTL=">nul
+    if %errorlevel% == 0 (
+        echo %package_name% Was Successfully Removed
+        goto eof
+    ) else (
+        echo An Error Occured
+        goto eof
+    )
+)
+goto eof
+
+:def_installed_packages
+set counter=0
+echo.
+echo Creation Date    Package Name
+echo -----------------------------
+for %%A in ("C:\users\%username%\.unwrap\.temp\memory\*") do (
+    set /a counter+=1
+    set file_date=%%~tA
+    echo  !file_date:~0,10!      %%~nA
+)
+echo.
+echo    Total Packages: !counter!
+goto eof
+
+:def_pkg_bool
+if exist C:\users\%username%\.unwrap\.temp\memory\%package_name%.mem (
+    echo Package Actuality: [92mTrue[0m
+) else (
+    echo Package Actuality: [91mFalse[0m
+)
+goto eof
+
+
 :def_show_more
 goto eof
 echo ⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⡀⠀⠀⠀⠀⠀⠀⢀⣀⡀⠀⠀⠀⠀⠀
@@ -368,3 +508,4 @@ echo.
 echo welcome to unwrap package manager, a package manager for windows scripts, pyscripts, vimscripts, etc...
 goto eof
 :eof
+title %username%
